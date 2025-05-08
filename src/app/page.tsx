2109fc34ebc20bc9bf48f8df949b0b63db7e87ef
KangSujin <tnwls7138@gmail.com>
2025-05-09 08:03:18 +0900

    update : prettier, Eslint 설정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,3 @@
-import Image from "next/image";
-
 export default function Home() {
-  return (
-    <div>
-      <header>
-        <div></div>
-      </header>
-    </div>
-  );
+  return <div></div>;
 }
